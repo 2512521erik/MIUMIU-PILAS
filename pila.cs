6bad474a6a0701e643813d79f413555da79bd9e2
eriksiño <UPN@ULCLAB1326>
2025-01-21 12:00:37 -0500

    avance2

---
+++ pila.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIUMIU1 {
+    internal class pila 
+    {
+        private nodo cima = null;
+        private int contador = 0;
+        public void push (string nom) 
+        {
+            nodo nuevo = new nodo();
+            nuevo.Nombre=nom;
+            nuevo.Siguiente=cima;
+            cima = nuevo;
+            contador++;
+        }
+        public void mostrar (ListBox list) 
+        {
+            nodo 
+        }
+    }
+}
